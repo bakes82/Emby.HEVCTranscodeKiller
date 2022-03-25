82ec9f96e96c787368e90fd6ce36566c8b662884
Brad <Bradley.j.baker.1982@gmail.com>
2022-03-25 00:41:55 -0400

    add gitignore

--- Emby.HEVCTranscodeKiller/obj/Debug/netstandard2.0/Emby.HEVCTranscodeKiller.AssemblyInfo.cs
+++ Emby.HEVCTranscodeKiller/obj/Debug/netstandard2.0/Emby.HEVCTranscodeKiller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Emby.HEVCTranscodeKiller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Emby.HEVCTranscodeKiller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Emby.HEVCTranscodeKiller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
